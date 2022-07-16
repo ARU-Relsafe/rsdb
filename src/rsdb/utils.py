eadc2be040a1860d0395a314268e0e3db98990b0
Vladimir Kravchenko <1783486+HexQuant@users.noreply.github.com>
2022-07-16 11:13:51 +0300

    Add functions

--- src/rsdb/utils.py
+++ src/rsdb/utils.py
@@ -1,4 +1,6 @@
-import orm
+import rsdb_.orm as orm
+from scipy.stats import beta, gamma, lognorm, norm
+import numpy as np
 
 def up_while_or(node: orm.FTNode) -> orm.FTNode|None:
     """Поднимаемся наверх по дереву пока встречаются ИЛИ гейты или начало дерева отказов"""
@@ -13,6 +15,34 @@ def up_while_or(node: orm.FTNode) -> orm.FTNode|None:
             return result
     return None
 
-def et_propagate(event_tree: orm.EventTree, back_propagate=False, recursive=False)->List[orm.EventTree]:
-    
+def et_propagate(event_tree: orm.EventTree, back_propagate=False, recursive=False):
     pass
+
+def get_distribution(param: orm.Param):
+    """
+    The function returns the distribution object of scipy
+    """
+    if param.DistType==orm.DistributionEnum.Gamma.value:
+        # для гамма распределения
+        # a-shape (k)
+        # scale-theta scale (mean=shape*scale)
+        return gamma(a=param.DistPar1, scale=param.DistPar1/param.Mean)
+    elif param.DistType==orm.DistributionEnum.Beta.value:
+        # для бета распределения
+        # a-alpha shape
+        # b-beta shape (mean=alpha/(alpha-beta)
+        return beta(a=param.DistPar1, b=param.DistPar1/param.Mean-param.DistPar1)
+    elif param.DistType==orm.DistributionEnum.Lognormal.value:
+        # для лог-нормлаьного распределения
+        # s-среднеквадратическое отклонение (EF=p95/p50) (sigma=log(EF)/CI90)
+        # scale-медиана (scale=exp(mu))
+        ef = param.DistPar1
+        m = param.Mean
+        ci90 = 1.64485363
+        sigma = np.log(ef)/ci90
+        med = m*np.exp(-(sigma**2)/2)
+        return lognorm(s=sigma, scale = med)
+    elif param.DistType==orm.DistributionEnum.Normal.value:
+        return norm(loc=param.Mean, scale=param.DistPar1)
+    else:
+        return None
